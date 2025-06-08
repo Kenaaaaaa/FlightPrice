1465f5b8eacdf7452964fcc89579be409bfd7ab2
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-09 00:50:29 +0200

    annotations

---
+++ src/main/java/core/db/annotations/Id.java
@@ -0,0 +1,12 @@
+
+// core/db/annotations/Id.java
+package core.db.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Id { }
