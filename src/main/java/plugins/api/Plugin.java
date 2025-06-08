b8548d6f1993565a4a2bf908f93382e4a597b43c
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-09 00:57:41 +0200

    Plugins

---
+++ src/main/java/plugins/api/Plugin.java
@@ -0,0 +1,13 @@
+// plugins/api/Plugin.java
+package plugins.api;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Plugin {
+    String name();
+}
---
+++ src/main/java/plugins/api/PricingRule.java
@@ -0,0 +1,13 @@
+// plugins/api/PricingRule.java
+package plugins.api;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface PricingRule {
+    int priority() default 0;
+}
